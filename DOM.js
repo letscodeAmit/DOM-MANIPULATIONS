57a1e4dccbeaf258c9f908bfe953434972c210fe
ursmrt <amitursmrt@gmail.com>
2022-10-16 01:19:26 -0700

    task5vg

--- DOM.js
+++ DOM.js
@@ -39,20 +39,43 @@
 
 //GET ELEMENTS BY CLASS NAME//
 
-var items= document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent='Hello 2';
-items[1].style.fontWeight='bold';
-items[1].style.backgroundColor='yellow';
-// items.style.backgroundColor='f4f4f4'; GIVES ERROR
-for(var i = 0; i<items.length;i++)
-{
-    items[i].style.backgroundColor='#f4f4f4';
-}
+// var items= document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent='Hello 2';
+// items[1].style.fontWeight='bold';
+// items[1].style.backgroundColor='yellow';
+// // items.style.backgroundColor='f4f4f4'; GIVES ERROR
+// for(var i = 0; i<items.length;i++)
+// {
+//     items[i].style.backgroundColor='#f4f4f4';
+// }
 
-items[2].style.backgroundColor='green';
-for(var i=0; i<items.length;i++)
-{
-    items[i].style.fontWeight='bold';
-}
+// items[2].style.backgroundColor='green';
+// for(var i=0; i<items.length;i++)
+// {
+//     items[i].style.fontWeight='bold';
+// }
+// get element by tag name
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent='Hello 2';
+// li[1].style.fontWeight='bold';
+// li[1].style.backgroundColor='yellow';
+
+// for(var i=0; i<FileList.length;i++)
+// {
+//     li[i].style.backgroundColor='#f4f4f4';
+// }
+
+//QUERYSELECTOR// ONLY FOR 1 ITEM
+
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
+var li = document.getElementsByTagName('li');
+li[4].style.color='red';
+var items= document.getElementsByClassName('list-group-item');
+items[1].style.color='green';
+items[2].style.backgroundColor=('pink');
+
