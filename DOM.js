3d628f3e64adf7934cb84ea5505880dad703bc3c
ursmrt <amitursmrt@gmail.com>
2022-10-29 00:50:27 -0700

    task6

--- DOM.js
+++ DOM.js
@@ -71,10 +71,33 @@
 
 //QUERYSELECTOR// ONLY FOR 1 ITEM
 
-var header=document.querySelector('#main-header');
-header.style.borderBottom='solid 4px #ccc';
-var li = document.getElementsByTagName('li');
-li[4].style.color='red';
-var items= document.getElementsByClassName('list-group-item');
-items[1].style.color='green';
-items[2].style.backgroundColor=('pink');
+// var header=document.querySelector('#main-header');
+// header.style.borderBottom='solid 4px #ccc';
+// var li = document.getElementsByTagName('li');
+// li[3].style.color='red';
+ var items= document.getElementsByClassName('list-group-item');
+ items[1].style.color='green';
+// items[2].style.backgroundColor=('pink');
+
+// var input = document.querySelector('input');
+// input.value='Hello World'
+// var submit=document.querySelector('input[type="submit"]');
+
+// submit.value='SEND'
+// var item=document.querySelector('.list-group-item');
+// item.style.color='red';
+// var lastItem=document.querySelector('.list-group-item:last-child');
+// lastItem.style.color='blue';
+
+// var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color='coral';
+
+//QUERY SELECTOR ALL//
+var titles=document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent='Hello';
+var p=document.querySelectorAll('li:nth-child(odd)');
+for( var i=0; i<p.length; i++)
+{
+    p[i].style.backgroundColor='green';
+}
