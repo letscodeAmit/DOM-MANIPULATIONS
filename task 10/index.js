715bc0ba6c488f45fb992914bc8f13c0f3864863
ursmrt <amitursmrt@gmail.com>
2022-11-04 12:50:29 +0530

    task11

--- task 10/index.js
+++ task 10/index.js
@@ -17,8 +17,11 @@ function saveToLocalStorage(event){
     event.preventDefault();
     const name=event.target.username.value;
     const email=event.target.emailId.value;
-    
-    localStorage.setItem('name',name);
-    localStorage.setItem('email',email);
-
+    //  localStorage.setItem('name',name);
+    // localStorage.setItem('email',email);
+const obj={
+        name,
+        email,
+    }
+    localStorage.setItem(obj.email,JSON.stringify(obj));
 }
