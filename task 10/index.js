a147a7225ddd951d93a5af86e6666bff5c4a5812
ursmrt <amitursmrt@gmail.com>
2022-11-06 18:16:52 +0530

    task13

--- task 10/index.js
+++ task 10/index.js
@@ -1,27 +1,34 @@
-// localStorage.setItem('name','Bob')
-// console.log(localStorage.getItem('name'))
-// //localStorage.removeItem('name')
-
-
-// sessionStorage.setItem('name','john')
-// //console.log(sessionStorage.getItem('name'))
-// //sessionStorage.removeItem('name')
-// sessionStorage.setItem('name','Bob')
-
-// document.cookie='name=kyle; expires=' + new Date(2032,0,1).toUTCString()
-// document.cookie='lastName=Smith; expires=' + new Date(2032,0,1).toUTCString()
-
-// console.log(document.cookie)
-
 function saveToLocalStorage(event){
     event.preventDefault();
     const name=event.target.username.value;
     const email=event.target.emailId.value;
-    //  localStorage.setItem('name',name);
-    // localStorage.setItem('email',email);
+  
 const obj={
         name,
         email,
     }
     localStorage.setItem(obj.email,JSON.stringify(obj));
+
+showNewUserOnSreen(obj)
+}
+
+window.addEventListener("DOMContentLoaded", () => { 
+
+    const localStorageObj = localStorage;
+    const localStoragekeys = Object.keys(localStorageObj);
+  
+    for(var i=0; i<localStoragekeys.length;i++){
+
+      const key = localStoragekeys[i];
+      const userDetailsString = localStorageObj[key];
+      const userDetailsObj = JSON.parse(userDetailsString);
+      showNewUserOnSreen(userDetailsObj)
+    }
+});
+
+function showNewUserOnSreen(user){
+
+    const parentNode=document.getElementById('listOfUsers');
+    const childHTML=`<li>${user.name} - ${user.email}</li>`
+    parentNode.innerHTML = parentNode.innerHTML + childHTML;
 }
